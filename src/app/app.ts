48ba0fced05f5e7fc55f22919922a2e86482605f
Aimnor <m.fabre123@gmail.com>
2025-07-30 18:42:28 +0200

    change professionnals object place

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProfessionnalsViewer } from "./professionnals-viewer/professionnals-viewer";
-import { CalendarComponent } from './professionnals-viewer/calendar.component';
+import { ProfessionalsService } from './professionnals-viewer/professionnal.service';
+import { Professional } from './professionnals-viewer/professional.model';
+import { Observable } from 'rxjs';
+import { DayPilot } from '@daypilot/daypilot-lite-angular';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,35 @@ import { CalendarComponent } from './professionnals-viewer/calendar.component';
 })
 export class App {
   protected readonly title = signal('beer_agenda_website');
+  professionals: Professional[] = [];
+  datePattern: string = "yyyy/MM/dd HH:mm"
+
+  constructor(private ps: ProfessionalsService) {
+  }
+  ngOnInit() {
+    const professionals$: Observable<Professional[]> = this.ps.getProfessionnals();
+    professionals$.subscribe({
+      next: (v) => this.professionals = v,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
+  }
+
+  findSubscribed(): Professional[] {
+    return this.professionals.filter(p => p.subscribed).sort((a, b) => (a.name < b.name ? -1 : 1));
+  }
+
+  findNotSubscribed(): Professional[] {
+    return this.professionals.filter(p => !p.subscribed).sort((a, b) => (a.name < b.name ? -1 : 1));
+  }
+
+  getEvents(): DayPilot.EventData[] {
+    return this.professionals.flatMap(p => p.events.flatMap(
+      e => <DayPilot.EventData>{
+        start: DayPilot.Date.parse(e.date, this.datePattern),
+        end: DayPilot.Date.parse(e.date, this.datePattern).addHours(2),
+        id: 0,
+        text: e.name
+      }));
+  }
 }
--- src/app/professionnals-viewer/professionnals-viewer.ts
+++ src/app/professionnals-viewer/professionnals-viewer.ts
@@ -1,8 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Professional } from './professional.model';
-import { DayPilot } from '@daypilot/daypilot-lite-angular';
-import { ProfessionalsService } from './professionnal.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-professionnals-viewer',
@@ -12,36 +9,6 @@ import { Observable } from 'rxjs';
 })
 
 export class ProfessionnalsViewer {
-  professionals: Professional[] = [];
-  datePattern: string = "yyyy/MM/dd HH:mm"
-
-  constructor(private ps: ProfessionalsService) {
-  }
-  ngOnInit() {
-    const professionals$: Observable<Professional[]> = this.ps.getProfessionnals();
-    professionals$.subscribe({
-      next: (v) => this.professionals = v,
-      error: (e) => console.error(e),
-      complete: () => console.info('complete')
-    });
-  }
-
-  findSubscribed(): Professional[] {
-    return this.professionals.filter(p => p.subscribed).sort((a, b) => (a.name < b.name ? -1 : 1));
-  }
-
-  findNotSubscribed(): Professional[] {
-    return this.professionals.filter(p => !p.subscribed).sort((a, b) => (a.name < b.name ? -1 : 1));
-  }
-
-  getEvents(): DayPilot.EventData[] {
-    return this.professionals.flatMap(p => p.events.flatMap(
-      e => <DayPilot.EventData>{
-        start: DayPilot.Date.parse(e.date, this.datePattern),
-        end: DayPilot.Date.parse(e.date, this.datePattern).addHours(2),
-        id: 0,
-        text: e.name
-      }));
-  }
+  @Input() professionals: Professional[] = [];
 }
 
